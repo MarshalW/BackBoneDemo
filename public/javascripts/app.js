9c262947c619f39461c055aa8698136afa7092e1
Marshal Wu <marshal.wu@gmail.com>
2012-03-23 18:59:56 +0800

    v0.8

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,55 +1,51 @@
 $(document).ready(function () {
-    var Item = Backbone.Model.extend({
-        url:'/items'
-    });
-
-    var item = new Item();
-    item.set(
-        {
-            'title':'stabilo彩色铅笔',
-            price:12.4
-        });
-
-    $.ajax({
-        url:'data.txt',
-        success:function (data) {
-            item.set('imgData', data);
+    var ToolBarItem = Backbone.Model.extend({
+        initialize:function (title) {
+            this.title = title;
         }
     });
 
-    var ItemView = Backbone.View.extend({
-        el:$('#itemContent'),
-        template:$('#item-modify-tmpl').template(),
-
-        initialize:function (model) {
-            console.log('init item view.');
-            this.model=model;
-            var el=this.el;
-            this.model.on('change:imgData',function(){
-                $(el).find('#image')[0].src=model.get('imgData');
-            });
-        },
-
-        events:{
-            'change .itemTitle':'changeTitle',
-            'click .submit':'save'
-        },
-
-        changeTitle:function () {
-            this.model.set('title', ($(this.el).find('.itemTitle').first().val()));
-        },
-
-        save:function () {
-            this.model.save();
-        },
-
+    var ToolBarItemView = Backbone.View.extend({
         render:function () {
-            console.log('render');
-            $.tmpl(this.template, this.model).appendTo(this.el);
+            $('<button class="toolBarItem">' + this.model.title + '</button>').appendTo($('#toolBar'));
             return this;
+        },
+        events:{
+            "click":"action"
+        },
+        action:function () {
+            console.log(model.title);
+        },
+        initialize:function (model) {
+            this.model = model;
+            this.render();
         }
     });
 
-    var itemView = new ItemView(item);
-    itemView.render();
+    var ToolBar = Backbone.Collection.extend({
+        model:ToolBarItem
+    });
+
+    var ToolBarView = Backbone.View.extend({
+        el:$('#workSpace'),
+        render:function () {
+            $(this.el).empty();
+            $('<div class="toolBar" id="toolBar"></div>').appendTo($(this.el));
+        },
+        initialize:function (model) {
+            this.model = model;
+            this.render();
+
+            this.model.on('add', function (item) {
+                new ToolBarItemView(item);
+            });
+        }
+    });
+
+    var toolBar = new ToolBar();
+    var toolBarView = new ToolBarView(toolBar);
+
+    toolBar.add(new ToolBarItem('文字'));
+    toolBar.add(new ToolBarItem('图片'));
+    toolBar.add(new ToolBarItem('保存'));
 });
